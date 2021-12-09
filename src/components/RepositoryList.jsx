02b9154df5868cbf482c046463500b3c34c97535
Thomas Dixini <thomasdixini@gmail.com>
2021-12-08 23:58:23 -0300

    Adicionando o primeiro componente React, (Lista de Repositórios)

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+
+
+export function RepositoryList(){
+    return (
+    <div>
+        <h1>Lista de Repositoriso</h1>
+        <ul>
+            <li>
+                <strong>unform</strong>
+                <p>Forms in React</p>
+                <a href="">Algum Link</a>
+            </li>
+            <li>
+                <strong>unform</strong>
+                <p>Forms in React</p>
+                <a href="">Algum Link</a>
+            </li>
+            <li>
+                <strong>unform</strong>
+                <p>Forms in React</p>
+                <a href="">Algum Link</a>
+            </li>
+            <li>
+                <strong>unform</strong>
+                <p>Forms in React</p>
+                <a href="">Algum Link</a>
+            </li>
+            
+        </ul>
+    </div> );
+    
+}
